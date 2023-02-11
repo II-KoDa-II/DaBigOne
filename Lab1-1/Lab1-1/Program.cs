136b1a69f9f4f129a920b41210d644e5b180d149
Koff Daniil <119159791+II-KoDa-II@users.noreply.github.com>
2023-02-11 22:57:06 +0700

    Add files via upload

---
+++ Lab1-1/Lab1-1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*******************************
+ *                             *
+ *  Кофф Даниил ПИ-221         *
+ *  Лаборторная работа 1-1     *
+ *                             *
+ *******************************/
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aNaturalNumber;
+            int nPower;
+            int result = 1;
+
+            Console.WriteLine("Enter value for variable a: ");
+            aNaturalNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter value for power variable n: ");
+            nPower = Convert.ToInt32(Console.ReadLine());
+
+            for (int index = 0; index < nPower; ++index)
+            {
+                result *= aNaturalNumber;
+            }
+
+            Console.WriteLine("\n" + aNaturalNumber + " in power of " + nPower + " equals: " + result);
+            Console.ReadKey();
+        }
+    }
+}
